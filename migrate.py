db77b9f57bf8ef09e167bfbec956c69cde836a99
xarion <erdicalli@gmail.com>
2016-04-15 14:45:40 +0200

    added migrate.py to handle database migrations

---
+++ migrate.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
+from run import app
+from model import db
+
+migrate = Migrate(app, db)
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
+if __name__ == '__main__':
+    manager.run()
